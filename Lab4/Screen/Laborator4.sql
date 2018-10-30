5c83b3e7471224942a4370be7f6e7a961d9b9bfd
Vasilica <43186373+boaghivasile@users.noreply.github.com>
2018-10-30 23:11:40 +0200

    Add files via upload

--- Lab4/Screen/Laborator4.sql
+++ Lab4/Screen/Laborator4.sql
@@ -2,11 +2,12 @@
 SELECT *
 FROM grupe
 /* 35.Gasiti denuumirile disciplinelor si media notelor pe disciplina. Afisati numai disciplinele cu medii mai mari ca 7.0. */
-SELECT distinct dis.Disciplina, (SELECT AVG(sr1.NOTA) from studenti_reusita AS sr1 WHERE sr.Id_Disciplina = sr1.Id_Disciplina) as Media
+SELECT distinct dis.Disciplina, (SELECT AVG(CAST(sr1.NOTA AS FLOAT)) 
+FROM studenti_reusita AS sr1 WHERE sr.Id_Disciplina = sr1.Id_Disciplina) as Media
 FROM studenti_reusita AS sr
-JOIN discipline as dis
+JOIN discipline AS dis
 ON sr.Id_Disciplina = dis.Id_Disciplina
-WHERE  (SELECT AVG(sr1.Nota) from studenti_reusita AS sr1 WHERE sr.Id_Disciplina = sr1.Id_Disciplina) > 7
+WHERE (SELECT AVG(CAST(sr1.Nota AS FLOAT)) from studenti_reusita AS sr1 WHERE sr.Id_Disciplina = sr1.Id_Disciplina) > 7
 /* 27.Afisati studentii (identificatorii) care au sustinut (evaluarea examen) la toate disciplinele predate de prof.Ion. */
 SELECT distinct sr.Id_Student
 FROM studenti_reusita as sr
